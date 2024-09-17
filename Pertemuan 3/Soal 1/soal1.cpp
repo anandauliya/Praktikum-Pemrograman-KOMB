534037e7f2a1fa0a9b9f930261ff2fd7ba74b4c6
anandauliya <anandaauliyarahma@mail.ugm.ac.id>
2024-09-17 16:34:43 +0700

    Update soal1.cpp

--- Pertemuan 3/Soal 1/soal1.cpp
+++ Pertemuan 3/Soal 1/soal1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+	double a,b,c;
+	double diskriminan, x1, x2;
+	
+	cout << "Masukkan nilai a: ";
+	cin >> a;
+	
+	cout << "Masukkan nilai b: ";
+	cin >> b;
+	
+	cout << "Masukkan nilai c: ";
+	cin >> c;
+		
+	diskriminan = b*b - 4*a*c;
+	
+	if (diskriminan > 0){
+		x1 = (-b + sqrt(diskriminan))/(2*a);
+		x2 = (-b - sqrt(diskriminan))/(2*a);
+		cout << "Dua akar real yang berbeda" << endl;
+		cout << "Akar 1: " << x1 << endl;
+		cout << "Akar 2: " << x2 << endl;
+}
+	else if (diskriminan == 0){
+		x1 = -b/(2*a);
+		cout << "Akar real yang sama" << endl;
+		cout << "Akar: " << x1 << endl;	
+}
+	else if (diskriminan < 0){
+		cout << "Tidak memiliki akar real" << endl;
+}
+return 0;
+	}
