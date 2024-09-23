4018f3ff4bbc7e9fc0831fec5577f701c4f64a50
anandauliya <anandaauliyarahma@mail.ugm.ac.id>
2024-09-23 19:57:17 +0700

    Update multiplication.cpp

--- Pertemuan 4/Soal 2/multiplication.cpp
+++ Pertemuan 4/Soal 2/multiplication.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+	cout << "Input = ";
+	cin >> n;
+	cout << "Output = " << endl;
+    for (int i = 1; i <= 10; ++i) 
+        cout << n << " * " << i << " = " << n * i << endl;
+    return 0;
+}
