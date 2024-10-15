0f8effb780d58c5a4903a0ef2b0dcf7d76161d02
anandauliya <anandaauliyarahma@mail.ugm.ac.id>
2024-10-15 20:40:57 +0700

    Update multiplication.cpp

--- Pertemuan 4/Soal 2/multiplication.cpp
+++ Pertemuan 4/Soal 2/multiplication.cpp
@@ -4,10 +4,24 @@ using namespace std;
 int main()
 {
     int n;
-	cout << "Input = ";
-	cin >> n;
+	int input = 5;
+    int expected_output[] = {5, 10, 15, 20, 25};
+    
+    bool test_passed = true;
+    for (int i = 0; i < 5; ++i) {
+        if (input * (i + 1) != expected_output[i]) {
+            test_passed = false;
+            break;
+        }
+    }
+    
+    if (test_passed) {
+        cout << "Test Passed" << endl;
+    } else {
+        cout << "Test Failed" << endl;
 	cout << "Output = " << endl;
     for (int i = 1; i <= 10; ++i) 
         cout << n << " * " << i << " = " << n * i << endl;
     return 0;
+    }
 }
