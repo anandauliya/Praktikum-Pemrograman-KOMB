67d03bcb1a4effd26dcff367bf4653218ada5503
anandauliya <anandaauliyarahma@mail.ugm.ac.id>
2024-10-15 22:27:59 +0700

    Update soal1.cpp

--- Pertemuan 2/Soal 1/soal1.cpp
+++ Pertemuan 2/Soal 1/soal1.cpp
@@ -3,27 +3,15 @@
 using namespace std;
 
 int main() {
-    string name;
-    double baseSalary, percentage, installment, insurance;
-    double grossSalary, tax, netSalary;
-    cout << "Enter employee name: ";
-    getline(cin,name);
+    string name = "Mail";
+    double baseSalary = 15000000;
+    double percentage = 100;
+    double installment = 200000;
+    double insurance = 150000;
 
-    cout << "Enter base salary: ";
-    cin >> baseSalary;
-
-    cout << "Enter gross salary percentage: ";
-    cin >> percentage;
-
-    cout << "Enter fixed installment amount: ";
-    cin >> installment;
-
-    cout <<"Enter fixed insurance amount: ";
-    cin >> insurance;
-
-    grossSalary = (percentage / 100) * baseSalary;
-    tax = 0.20 * grossSalary;
-    netSalary = grossSalary - tax - installment - insurance;
+    double grossSalary = (percentage / 100) * baseSalary;
+    double tax = 0.20 * grossSalary;
+    double netSalary = grossSalary - tax - installment - insurance;
 
     cout << "========== PAYSLIP ==========" << endl;
     cout << "Employee Name: " << name << endl;
@@ -34,5 +22,15 @@ int main() {
     cout << "Net Salary: " << fixed <<setprecision(2) << netSalary << endl;
     cout << "=============================" << endl;
 
+    double expectedGrossSalary = 15000000;
+    double expectedTax = 3000000;
+    double expectedNetSalary = 11650000;
+
+    
+    if (grossSalary == expectedGrossSalary && tax == expectedTax && netSalary == expectedNetSalary) {
+        cout << "Test Passed" << endl;
+    } else {
+        cout << "Test Failed" << endl;
+    }
     return 0;
 }
